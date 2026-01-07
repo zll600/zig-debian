18591953825f5c9d35f189b7c03c5a1fd51dca2c
zll600 <3400692417@qq.com>
2026-01-07 19:48:32 +0800

    Support aarch64

--- build_zig_debian.sh
+++ build_zig_debian.sh
@@ -1,19 +1,41 @@
+#!/bin/bash
 ZIG_VERSION=$1
 BUILD_VERSION=$2
-declare -a arr=("bookworm" "trixie" "forky" "sid")
-for i in "${arr[@]}"
+TARGET_ARCH=$3
+
+declare -a dists=("bookworm" "trixie" "forky" "sid")
+
+for dist in "${dists[@]}"
 do
-  DEBIAN_DIST=$i
-  FULL_VERSION=$ZIG_VERSION-${BUILD_VERSION}+${DEBIAN_DIST}_amd64
-  
-  docker build . -t zig-$DEBIAN_DIST --build-arg ZIG_VERSION=$ZIG_VERSION --build-arg DEBIAN_DIST=$DEBIAN_DIST --build-arg BUILD_VERSION=$BUILD_VERSION --build-arg FULL_VERSION=$FULL_VERSION -f meta_Dockerfile
-  id="$(docker create zig-$DEBIAN_DIST)"
-  docker cp $id:/zig_$FULL_VERSION.deb - > ./zig_$FULL_VERSION.deb
-  tar -xf ./zig_$FULL_VERSION.deb
+  DEBIAN_DIST=$dist
 
-  docker build . -t zig-$DEBIAN_DIST --build-arg ZIG_VERSION=$ZIG_VERSION --build-arg DEBIAN_DIST=$DEBIAN_DIST --build-arg BUILD_VERSION=$BUILD_VERSION --build-arg FULL_VERSION=$FULL_VERSION -f zero_Dockerfile
-  id="$(docker create zig-$DEBIAN_DIST)"
-  docker cp $id:/zig-zero_$FULL_VERSION.deb - > ./zig-zero_$FULL_VERSION.deb
-  tar -xf ./zig-zero_$FULL_VERSION.deb
+  if [ "$arch" = "arm64" ]; then
+    ZIG_ARCH="aarch64"
+  else
+    ZIG_ARCH="x86_64"
+  fi
 
+  FULL_VERSION=$ZIG_VERSION-${BUILD_VERSION}+${DEBIAN_DIST}_${TARGET_ARCH}
+
+  docker build . -t zig-$DEBIAN_DIST-$TARGET_ARCH \
+    --build-arg ZIG_VERSION=$ZIG_VERSION \
+    --build-arg DEBIAN_DIST=$DEBIAN_DIST \
+    --build-arg BUILD_VERSION=$BUILD_VERSION \
+    --build-arg FULL_VERSION=$FULL_VERSION \
+    --build-arg DEBIAN_ARCH=$TARGET_ARCH \
+    --build-arg ZIG_ARCH=$ZIG_ARCH \
+    -f meta_Dockerfile
+  id="$(docker create zig-$DEBIAN_DIST-$TARGET_ARCH)"
+  docker cp $id:/zig_$FULL_VERSION.deb ./zig_$FULL_VERSION.deb
+
+  docker build . -t zig-zero-$DEBIAN_DIST-$TARGET_ARCH \
+    --build-arg ZIG_VERSION=$ZIG_VERSION \
+    --build-arg DEBIAN_DIST=$DEBIAN_DIST \
+    --build-arg BUILD_VERSION=$BUILD_VERSION \
+    --build-arg FULL_VERSION=$FULL_VERSION \
+    --build-arg DEBIAN_ARCH=$TARGET_ARCH \
+    --build-arg ZIG_ARCH=$ZIG_ARCH \
+    -f zero_Dockerfile
+  id="$(docker create zig-zero-$DEBIAN_DIST-$TARGET_ARCH)"
+  docker cp $id:/zig-zero_$FULL_VERSION.deb ./zig-zero_$FULL_VERSION.deb
 done
